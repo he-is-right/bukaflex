f279bda7c9cd7974fa148e56d8120e9251ce5d89
Timothy Dawadakpoye <timdawadakpoye@gmail.com>
2024-09-03 06:17:24 +0100

    Fixing static files
    Fixing static files to add styling

--- build.sh
+++ build.sh
@@ -5,6 +5,8 @@ set -o errexit
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
+# python manage.py collectstatic --noinput
+
 python manage.py migrate
 if [[ $CREATE_SUPERUSER ]];
 then
