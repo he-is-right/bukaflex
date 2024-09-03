ec59ce09c191be562564c003966cf606792685fd
Timothy Dawadakpoye <timdawadakpoye@gmail.com>
2024-09-03 05:48:41 +0100

    Update settings.py

--- bukaflex/settings.py
+++ bukaflex/settings.py
@@ -109,7 +109,7 @@ STATIC_URL = 'static/'
 # STATICFILES_DIRS = [
 #     BASE_DIR / "static"
 # ]
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # MEDIA_URL = '/media/'
