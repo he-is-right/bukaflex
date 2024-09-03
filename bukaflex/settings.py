84580c2f95f0b778e22bb6976860e47d091402fe
Timothy Dawadakpoye <timdawadakpoye@gmail.com>
2024-09-03 05:56:16 +0100

    Installed DRF
    Installed django rest framework

--- bukaflex/settings.py
+++ bukaflex/settings.py
@@ -27,6 +27,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Django Rest Framework
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
